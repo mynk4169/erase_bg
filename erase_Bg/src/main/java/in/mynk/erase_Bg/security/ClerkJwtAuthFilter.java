d8180db127d81946250da5d20f6dbd37d5ad46e1
mynk4169 <mayankthakre12@gmail.com>
2025-05-18 17:51:11 +0530

    adding in security.config

--- erase_Bg/src/main/java/in/mynk/erase_Bg/security/ClerkJwtAuthFilter.java
+++ erase_Bg/src/main/java/in/mynk/erase_Bg/security/ClerkJwtAuthFilter.java
@@ -35,10 +35,14 @@ public class ClerkJwtAuthFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
-        if (request.getRequestURI().contains("/api/webhooks")){
-            filterChain.doFilter(request,response);
+        String path = request.getRequestURI();
+
+        // Skip filter for public endpoints
+        if (path.startsWith("/api/webhooks") || path.startsWith("/api/orders/verify") || path.startsWith("/api/users")) {
+            filterChain.doFilter(request, response);
             return;
         }
+
 
         String authHeader = request.getHeader("Authorization");
         if (authHeader == null || !authHeader.startsWith("Bearer ")) {
